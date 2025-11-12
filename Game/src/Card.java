9ff37d0759f3b504fece91005477d3a4a7e9659d
dohe8975 <dohe8975@student.acitech.org>
2025-11-12 12:55:16 -0500

    completed card figuring out deck

---
+++ Game/src/Card.java
@@ -0,0 +1,29 @@
+
+
+public class Card {
+    public final String[] faces = {null, "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    public final String[] suits = {null, "Spades", "Hearts", "Clubs", "Diamonds"};
+
+    private final int face;
+    private final int suit;
+
+    public Card(int face, int suit) {
+        this.face = face;
+        this.suit = suit;
+    }
+
+    public int getFace() {
+        return face;
+    }
+
+    public int getSuit() {
+        return suit;
+    }
+
+    public String toString() {
+        return faces[face] + " of " + suits[suit];
+    }
+
+    public static void main(String[] args) {
+        Card card1 = new Card(1, 1); System.out.println(card1);
+    } }
---
+++ Game/src/Deck.java
@@ -0,0 +1,16 @@
+
+
+public class Deck {
+    public static void main(String[] args)
+    {
+        // initializing array
+        int[] arr = {40,10,25,63};
+
+        // size of array
+        int n = arr.length;
+
+        // traversing array
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
